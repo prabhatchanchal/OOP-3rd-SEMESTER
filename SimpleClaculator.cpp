48cf20b67f6e1da7c5eaa437c0ff27b75b1f9434
Prabhaat kumar <45708106+prabhatchanchal@users.noreply.github.com>
2019-09-01 19:29:38 +0530

    Update SimpleClaculator.cpp

--- SimpleClaculator.cpp
+++ SimpleClaculator.cpp
@@ -7,6 +7,7 @@ int main() {
   cout<<"you have to first enter the opration which you want to perform the two number\n";
   cout<<"exaple:-\n";
   cout<<"Enter the oprator \n+\ninter two number\n 8 9\n the sum of 8 and 9 is 17 will be printed\n oprators are + - / * %\n**************************************\n";
+  rerun:
   cout<<"Enter the oprator\n";
   cin>>oprator;
   cout<<"Enter two number\n";
@@ -29,7 +30,7 @@ int main() {
     break;
     default:
     cout<<"you entered wrong oprator\n";
-      main();
+      goto rerun;
   }
  return 0;
 }
